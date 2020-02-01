1b4a0963d2b42dcfb15655e42fdd8eb470228096
cp3 <792716300@qq.com>
2020-02-01 16:29:40 +0800

    两种简单得方法

--- src/algorithm/超过一半得数字.java
+++ src/algorithm/超过一半得数字.java
@@ -1,10 +1,28 @@
 package algorithm;
 
+import java.util.Arrays;
+
 public class 超过一半得数字 {
+	/**
+	 * 
+	 * 数组中有一个数字出现得次数超过了数组长度得一半，找出这个数字
+	 * 可以用快排得思想，也可以排序后找第n/2个数
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[]arr= {5,2,1,3,2,1,1,1,4,1,1};
+		System.out.println(solve1(arr));
+		System.out.println(solve2(arr));
+	}
+	
+	static int solve1(int[]arr) {
+		Arrays.sort(arr);
+		return arr[(arr.length)/2];
+		
+	}
+	static int solve2(int[]arr) {
+		int res=第k小得数.selectk(arr, 0, arr.length-1, arr.length/2);
+		return res;
 	}
 
 }
