704ca8c81ed72fc738b4565b3568a45bde80d843
cp3 <792716300@qq.com>
2020-02-01 17:08:20 +0800

    最小可用id

---
+++ src/algorithm/最小可用id.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+public class 最小可用id {
+
+	/**
+	 * 在非负数组（乱序）中找到最小的可分配的id（从1开始编号），数据量为1000000
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
