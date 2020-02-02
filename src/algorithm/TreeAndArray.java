54e17c1db02e90903e00cc451b51f4d2a51d5535
cp3 <792716300@qq.com>
2020-02-02 18:45:14 +0800

    树的遍历

--- src/algorithm/TreeAndArray.java
+++ src/algorithm/TreeAndArray.java
@@ -3,8 +3,26 @@ package algorithm;
 public class TreeAndArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[]arr= {78,56,34,43,4,1,15,2,23};
+		preOrder(arr, 0);
 
 	}
+	//前序遍历
+	static void preOrder(int[]arr,int index) {
+		if(index>=arr.length)return;
+		
+		System.out.println(arr[index]);//输出根节点
+		preOrder(arr, index*2+1);//输出左子树
+		preOrder(arr, index*2+2);//输出右子树
+	}
+	//中序遍历
+	static void inOrder(int[]arr,int index) {
+		if(index>=arr.length)return;
+		
+		preOrder(arr, index*2+1);//输出左子树
+		System.out.println(arr[index]);//输出根节点
+		preOrder(arr, index*2+2);//输出右子树
+	}
+	
 
 }
