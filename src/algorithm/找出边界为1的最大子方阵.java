437521b6ba48d5c34d51f94fc23b2dd2cec1e722
cp3 <792716300@qq.com>
2020-02-03 19:12:48 +0800

    找出边界为1的最大子方阵

--- src/algorithm/找出边界为1的最大子方阵.java
+++ src/algorithm/找出边界为1的最大子方阵.java
@@ -3,8 +3,56 @@ package algorithm;
 public class 找出边界为1的最大子方阵 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][]A= {
+				{1,1,1,1},
+				{1,1,0,0},
+				{1,1,1,1},
+				{1,1,1,1}
+				};
+		System.out.println(f(A));
+		
+	}
+	static int f(int[][]A) {
+		int N=A.length;
+		int n=N;
+		
+		while(n>0) {
+			
+			for(int i=0;i<N;i++) {
+				if(i+n>N)break;
+				l3:
+				for(int j=0;j<N;j++) {
+					if(j+n>N)break;
+					int r=i;
+					int c=j;
+					
+					//上
+					while(c<j+n) if(A[r][c]==0)continue l3;
+					else c++;
+					c--;
+					//下
+					while(r<i+n)if(A[r][c]==0)continue l3;
+					else r++;
+					r--;
+					//左
+					while(c>=j)if(A[r][c]==0)continue l3;
+					else c--;
+					c++;
+					//右
+					while(r>=i)if(A[r][c]==0)continue l3;
+					else r--;
+					r++;
+					
+					return n;
+				}
+			}
+			
+			
+			n--;
+		}
+		
+		return 1;
+		
 	}
 
 }
