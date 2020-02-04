236db004bbe6e0de1820590fa21f4f527e87da6d
cp3 <792716300@qq.com>
2020-02-04 15:23:34 +0800

    变形词

---
+++ src/第五章/变形词.java
@@ -0,0 +1,19 @@
+package 第五章;
+
+public class 变形词 {
+
+	
+	/**
+	 * 给定两个字符串，编写程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串
+	 * 规定大小写为不同的字符，且考虑字符中的空格
+	 * 测试样例：
+	 * "Here you are","Are you here"
+	 * 返回false
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
