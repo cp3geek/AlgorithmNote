5b3a648eb09f32fa8e2eea504f75333ca23cd506
cp3 <792716300@qq.com>
2020-02-04 14:45:36 +0800

    矩阵运算

--- src/algorithm/矩阵运算.java
+++ src/algorithm/矩阵运算.java
@@ -3,8 +3,45 @@ package algorithm;
 public class 矩阵运算 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][]a= {
+				{1,2,3},
+				{1,2,3},
+		};
+		int[][]b= {
+				{1,2},
+				{1,2},
+				{1,2}
+		};
+		
+		int[][]newm=f(a,b);
+		
+		for(int[]i:newm) {
+			for(int j:i) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	static int[][] f(int[][]a,int[][]b){
+		int m=a.length;
+		int n=a[0].length;
+		int N=b[0].length;
+		int[][]newMatrix=new int[m][N];
+		
+		
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<N;j++) {
+				for(int k=0;k<n;k++) {
+					newMatrix[i][j]=a[i][k]*b[k][j]+newMatrix[i][j];
+				}
+			}
+		}
+		
+		return newMatrix;
+		
+		
 	}
 
 }
