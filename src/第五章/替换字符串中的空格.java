255283ba19cc9403aa6288fb0fb5d0dcefbc9c43
cp3 <792716300@qq.com>
2020-02-04 15:44:53 +0800

    替换字符串中的空格

---
+++ src/第五章/替换字符串中的空格.java
@@ -0,0 +1,18 @@
+package 第五章;
+
+public class 替换字符串中的空格 {
+
+	/**
+	 * 将字符串中的空格全部替换为%20.假定该字符串有足够的空间存放新的字符并且知道
+	 * 字符串的真实长度小于1000，同时保证字符串由大小写的英文字母组成
+	 * 测试样例：
+	 * "Mr John Smith"
+	 * 返回:"Mr%20John%20Smith"
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
