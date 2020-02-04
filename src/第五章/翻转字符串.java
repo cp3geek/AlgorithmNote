58a5f2d4406d2fe67cc25120f14e6267f65706f7
cp3 <792716300@qq.com>
2020-02-04 15:20:10 +0800

    翻转字符串(两种方法)

--- src/第五章/翻转字符串.java
+++ src/第五章/翻转字符串.java
@@ -3,8 +3,23 @@ package 第五章;
 public class 翻转字符串 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(f("abc jkl"));
+		System.out.println(f1("this is a pig"));
+	}
+	
+	static String f(String str) {
+		StringBuilder sb=new StringBuilder(str);
+		return sb.reverse().toString();//运用api
+	}
+	//通过字符数组翻转
+	static String f1(String str) {
+		char[]newchar=str.toCharArray();
+		
+		
+		for(int i=0;i<str.length();i++) {
+			newchar[i]=str.charAt(str.length()-i-1);
+		}
+		return new String(newchar);
 	}
 
 }
