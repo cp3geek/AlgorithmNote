8b117554169c9d887145d1a9977a845e077883d1
cp3 <792716300@qq.com>
2020-02-04 16:21:37 +0800

    字符串压缩

---
+++ src/第五章/压缩字符串.java
@@ -0,0 +1,20 @@
+package 第五章;
+
+public class 压缩字符串 {
+
+	/**
+	 * 利用字符重复出现的次数，实现基本的字符串压缩功能
+	 * 比如字符串:"aabcccccaaa"压缩后变成a2b1c5a3,若压缩后的字符串长度没有变短，则返回原来的串
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+	
+	static String f(String str) {
+		
+		return "";
+	}
+
+}
