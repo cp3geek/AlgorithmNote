47fecee7d7c5243234679aa465cabf628e99c83f
cp3 <792716300@qq.com>
2020-02-04 16:17:53 +0800

    替换字符串中的空格(通用办法)

--- src/第五章/替换字符串中的空格.java
+++ src/第五章/替换字符串中的空格.java
@@ -11,8 +11,34 @@ public class 替换字符串中的空格 {
 	 * 
 	 */
 	public static void main(String[] args) {
-		
-
+		System.out.println(f("Mr John Smith"));
+		System.out.println(f1("Mr John Smith"));
+	}
+	
+	//使用java api，String里面的替换方法
+	static String f(String str) {
+		return str.replaceAll("\\s", "%20");//空格用\\s表示
+	}
+	
+	//通用方法，不使用api
+	static String f1(String str) {
+		int count=str.length()-1;
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)==' ')count+=3;
+		}
+		int size=count-1;
+		char[]s=new char[size];
+		int j=size-1;
+		for(int i=str.length()-1;i>=0;i--) {
+			if(str.charAt(i)==' ') {
+				s[j--]='0';
+				s[j--]='2';
+				s[j--]='%';
+			}else {
+				s[j--]=str.charAt(i);
+			}
+		}
+		return new String(s);
 	}
 
 }
