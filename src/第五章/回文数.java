6101cab040ab66691c992fa2c3f4936bd65165c9
cp3 <792716300@qq.com>
2020-02-06 17:53:40 +0800

    回文数（四位数特点,for循环嵌套，直接打印）

--- src/第五章/回文数.java
+++ src/第五章/回文数.java
@@ -8,20 +8,25 @@ public class 回文数 {
 	 * 
 	 */
 	public static void main(String[] args) {
-	f();	
-
+	//f();	
+	f1();
 	}
 	
 	static void f() {
-		
-		
 		for(Integer i=1000;i<10000;i++) {
 			StringBuilder sb=new StringBuilder(i.toString());
 			if(i.toString().equals(sb.reverse().toString())) 
 				System.out.println(i);
 		}
-		
-		
+	}
+	
+	//ijji;
+	static void f1() {
+		for(int i=1;i<10;i++) {
+			for(int j=0;j<10;j++) {
+				System.out.println(i*1000+j*100+j*10+i);
+			}
+		}
 	}
 
 }
