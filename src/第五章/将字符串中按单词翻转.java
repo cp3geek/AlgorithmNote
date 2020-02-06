a8c0637eb95cca6bc6be514ad6dfc92298305ef8
cp3 <792716300@qq.com>
2020-02-06 16:39:06 +0800

    将字符串按单词翻转

--- src/第五章/将字符串中按单词翻转.java
+++ src/第五章/将字符串中按单词翻转.java
@@ -7,8 +7,20 @@ public class 将字符串中按单词翻转 {
 	 * 
 	 */
 	public static void main(String[] args) {
-		
+		System.out.println(reverse("here you are"));
 
+	}
+	static String reverse(String str) {
+		String[]words=str.split("\\s");
+		StringBuilder sb=new StringBuilder();
+		
+		for(int i=words.length-1;i>=0;i--) {
+			sb.append(words[i]+" ");
+		}
+		
+		sb.deleteCharAt(sb.length()-1);
+		
+		return sb.toString();
 	}
 
 }
