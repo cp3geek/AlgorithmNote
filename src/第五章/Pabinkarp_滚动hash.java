54a63789dd9739e3142687ac1941f48538dc6c90
cp3 <792716300@qq.com>
2020-02-08 16:59:48 +0800

    字符串匹配（Pabinkarp_滚动hash）

--- src/第五章/Pabinkarp_滚动hash.java
+++ src/第五章/Pabinkarp_滚动hash.java
@@ -3,8 +3,52 @@ package 第五章;
 public class Pabinkarp_滚动hash {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String p="ABA";
+		String s="ABABABA";
+		match(p,s);
 
+	}
+	static final long seed=31;
+	
+	
+	static void match(String p,String s) {
+		long hash_p=hash(p);
+		long[]hashOfS=hash(s,p.length());
+		match(hash_p,hashOfS);
+	}
+	
+	private static long[] hash(String s, final int n) {
+		long[]res=new long[s.length()-n+1];
+		//前n个字符的hash
+		res[0]=hash(s.substring(0,n));
+		for(int i=n;i<s.length();i++) {
+			char newChar=s.charAt(i);
+			char oChar=s.charAt(i-n);
+			//前n个字符的hash*seed-前n个字符的第一个字符*seed的n次方
+			
+			long v=(res[i-n]*seed+newChar-(long)Math.pow(seed, n)*oChar)%Long.MAX_VALUE;
+			res[i-n+1]=v;
+		}
+		return res;
+	}
+
+	private static void match(long hash_p, long[] hash_s) {
+		for(int i=0;i<hash_s.length;i++) {
+			if(hash_p==hash_s[i]) {
+				System.out.println("match:"+i);
+			}
+		}
+		
+	}
+	
+	
+
+	static long hash(String str) {
+		long hash=0;
+		for(int i=0;i!=str.length();i++) {
+			hash=seed*hash+str.charAt(i);
+		}
+		return hash%Long.MAX_VALUE;
 	}
 
 }
