7177653fe8c4cdf92d90285da8ddcea0abdae273
cp3 <792716300@qq.com>
2020-02-08 17:58:16 +0800

    字符串匹配（KMP_暴力匹配，找出第一个匹配的子串的首下标）

--- src/第五章/字符串匹配之KMP_暴力匹配.java
+++ src/第五章/字符串匹配之KMP_暴力匹配.java
@@ -3,8 +3,28 @@ package 第五章;
 public class 字符串匹配之KMP_暴力匹配 {
 
 	public static void main(String[] args) {
-		
+		String s="babababcbabababb";
+		String p="bababb";
+		int i=match(s,p);
+		System.out.println(i);
 
+	}
+	
+	static int match(String s,String p) {
+		int i=0,j=0;int k=0;
+		while(i+p.length()<s.length()) {
+			if(s.charAt(k)==p.charAt(j)) {
+				j++;
+				k++;
+				if(j==p.length())return i;
+			}else {
+				i++;
+				j=0;
+				k=i;
+			}
+		}
+		
+		return -1;
 	}
 
 }
