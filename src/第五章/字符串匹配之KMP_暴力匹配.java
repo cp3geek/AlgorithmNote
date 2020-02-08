535132c41b5fd4ccf5d53300ac8840e7d7a0da41
cp3 <792716300@qq.com>
2020-02-08 18:56:54 +0800

    字符串匹配（KMP_暴力匹配）

--- src/第五章/字符串匹配之KMP_暴力匹配.java
+++ src/第五章/字符串匹配之KMP_暴力匹配.java
@@ -3,28 +3,55 @@ package 第五章;
 public class 字符串匹配之KMP_暴力匹配 {
 
 	public static void main(String[] args) {
-		String s="babababcbabababb";
-		String p="bababb";
-		int i=match(s,p);
-		System.out.println(i);
+		String s="ababababab";
+		String p="aba";
+
+//		System.out.println(match(s, p));
+		m(s,p);
+		
 
 	}
 	
 	static int match(String s,String p) {
-		int i=0,j=0;int k=0;
-		while(i+p.length()<s.length()) {
-			if(s.charAt(k)==p.charAt(j)) {
+		int i=0,j=0;int sc=i;
+		while(sc<s.length()) {
+			if(s.charAt(sc)==p.charAt(j)) {
 				j++;
-				k++;
+				sc++;
 				if(j==p.length())return i;
 			}else {
 				i++;
 				j=0;
-				k=i;
+				sc=i;
 			}
 		}
 		
 		return -1;
 	}
+	
+	
+	static void m(String s,String p) {
+		int i=0,j=0;int sc=i;
+		while(sc<s.length()) {
+			if(s.charAt(sc)==p.charAt(j)) {
+				j++;
+				sc++;
+				if(j==p.length()) {
+					System.out.println(i);
+					j=0;
+					i++;
+					sc=i;
+				}
+			}else {
+				i++;
+				j=0;
+				sc=i;
+			}
+		}
+	}
+	
+	
+	
+	
 
 }
