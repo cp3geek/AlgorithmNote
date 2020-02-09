a13cc93d9cea100bd2ddd385aa35df6a86e5f27c
cp3 <792716300@qq.com>
2020-02-09 16:52:53 +0800

    后缀数组

--- src/第五章/后缀数组.java
+++ src/第五章/后缀数组.java
@@ -2,6 +2,13 @@ package 第五章;
 
 public class 后缀数组 {
 
+	/**
+	 * 就是串得所有后缀子串按字典序排序后，在数组中记录后缀的起始下标
+	 * 后缀数组就是：排名和原下标的映射 sa[0]=5,起始下标为5的后缀在所有后缀中字典序最小
+	 *rank数组：给定后缀的下标，返回其字典序，rk[5]=0;
+	 *
+	 *子串：一定是某个后缀的前缀
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
