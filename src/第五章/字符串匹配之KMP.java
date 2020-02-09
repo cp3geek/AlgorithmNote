a699e70e97f0e120670c424f3db65923f2e17930
cp3 <792716300@qq.com>
2020-02-09 16:22:34 +0800

    字符串匹配之KMP（求next数组）

--- src/第五章/字符串匹配之KMP.java
+++ src/第五章/字符串匹配之KMP.java
@@ -39,13 +39,27 @@ public class 字符串匹配之KMP {
  * 字符串:bread
  * 前缀:b,br,bre,brea
  * 后缀:read,ead,ad,d 
- * 老师说的和这个有些不一样
+ * 然后next数组是求最大匹配得串
+ * 
  *
  * "前缀"指除了最后一个字符以外，一个字符串的全部头部组合；
  * "后缀"指除了第一个字符以外，一个字符串的全部尾部组合。
  */
 	
-	//递推求next数组/有疑惑
+	/**
+	 * babab b
+	 *前缀：b,ba,bab,baba,babab
+	 *后缀：abab,bab,ab,b
+	 *所以第6位得next数组中得值为3，因为前五位前缀后缀最大匹配串是bab 3位
+	 * 
+	 */
+	//递推求next数组
+	/**
+	 * 求该数组不可能每个子串得前缀后缀都去比较
+	 * 所以要逆推
+	 * 
+	 * 
+	 */
 	private static int[] next(String ps) {
 		int pLength=ps.length();
 		int[]next=new int[pLength];
@@ -55,8 +69,9 @@ public class 字符串匹配之KMP {
 		
 		next[1]=0;
 		int j=1;
-		int k=next[j];
+		int k=next[j];//看位置j得最长匹配前缀在哪里
 		while(j<pLength-1) {
+			//现在要推出next[j+1],检查j和k位置上得关系即可
 			if(k<0||p[j]==p[k]) {
 				next[++j]=++k;
 			}else {
