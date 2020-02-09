bbf28027f2e8bf45d1b333f63a24a9b40141b41e
cp3 <792716300@qq.com>
2020-02-09 15:04:09 +0800

    字符串匹配之KMP

---
+++ src/第五章/字符串匹配之KMP.java
@@ -0,0 +1,10 @@
+package 第五章;
+
+public class 字符串匹配之KMP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/练习/字符串匹配暴力匹配练习.java
+++ src/练习/字符串匹配暴力匹配练习.java
@@ -3,8 +3,53 @@ package 练习;
 public class 字符串匹配暴力匹配练习 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s="bababcbababababb";
+		String p="bababb";
+//		System.out.println(match(s,p));
+		m(new String("ababababab"),new String("aba"));
+	}
+	
+	
+	static int match(String s,String p) {
+		int i=0,j=0,sc=i;
+		
+		while(sc<s.length()) {
+			if(s.charAt(sc)==p.charAt(j)) {
+				sc++;
+				j++;
+				if(j==p.length()) {
+					return i;
+				}
+			}else {
+				i++;
+				sc=i;
+				j=0;
+			}
+		}
+		return -1;
+	}
+	
+	static void m(String s,String p) {
+		int i=0,j=0,sc=i;
+		
+		while(sc<s.length()) {
+			if(s.charAt(sc)==p.charAt(j)) {
+				sc++;
+				j++;
+				if(j==p.length()) {
+					System.out.println(i);
+					i++;
+					j=0;
+					sc=i;
+				}
+			}else {
+				i++;
+				sc=i;
+				j=0;
+			}
+		}
+		
+		
 	}
 
 }
