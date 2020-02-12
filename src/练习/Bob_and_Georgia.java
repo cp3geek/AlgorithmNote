d32cfc1837b489b2bd417947867b2a0465fc2273
cp3 <792716300@qq.com>
2020-02-12 14:51:43 +0800

    Bob and Georgia(练习)

---
+++ src/练习/Bob_and_Georgia.java
@@ -0,0 +1,22 @@
+package 练习;
+
+public class Bob_and_Georgia {
+
+	/**
+	 * * 输入：
+	 * 2//测试得组数
+	 * 3//该组得个数
+	 * 1 2 3//数据
+	 * 8
+	 * 1 5 6 7 9 12 14 17 
+	 *输出：
+	 *bob will win
+	 *georgia will win
+	 */
+	 
+	public static void main(String[] args) {
+		
+
+	}
+
+}
