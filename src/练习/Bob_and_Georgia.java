6b147e12db6dd93999c719d4cc3a31c4fa069d0a
cp3 <792716300@qq.com>
2020-02-12 15:01:17 +0800

    Bob and Georgia(练习)

--- src/练习/Bob_and_Georgia.java
+++ src/练习/Bob_and_Georgia.java
@@ -1,5 +1,7 @@
 package 练习;
 
+import java.util.Scanner;
+
 public class Bob_and_Georgia {
 
 	/**
@@ -15,8 +17,38 @@ public class Bob_and_Georgia {
 	 */
 	 
 	public static void main(String[] args) {
-		
+		Scanner input=new Scanner(System.in);
+		int caseNum=input.nextInt();
+		int[][]data=new int[caseNum][];
+		for(int i=0;i<caseNum;i++) {
+			int k=input.nextInt();
+			data[i]=new int[k];
+			for(int j=0;j<k;j++) {
+				data[i][j]=input.nextInt();
+			}
+		}
+		for(int i=0;i<caseNum;i++){
+			String res=deal(data[i]);
+			System.out.println(res);
+		}
 
+	}
+
+	private static String deal(int[] a) {
+		int len=a.length;
+		int res=0;
+		if((len&1)==1) {
+			for(int i=0;i<len;i+=2) {
+				res^=(i==0)?a[i]:(a[i]-a[i-1]);
+			}
+		}else {
+			for(int i=1;i<len;i+=2) {
+				res^=(a[i]-a[i-1]);
+			}
+		}
+		if(res==0)return "Bob will win";
+		else return "Georgia will win";
+		
 	}
 
 }
