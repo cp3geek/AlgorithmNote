6a2a651996f489dc74a0aaceb8c2e8f684a5c155
cp3 <792716300@qq.com>
2020-02-13 15:29:05 +0800

    欧几里得算法

--- src/第六章/欧几里得算法.java
+++ src/第六章/欧几里得算法.java
@@ -2,9 +2,17 @@ package 第六章;
 
 public class 欧几里得算法 {
 
+	/**
+	 * 就是辗转相除法
+	 * 求解最大公因子
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+	
+	public static int gcd(int m,int n) {
+		return n==0?m:gcd(n,m%n);
 	}
 
 }
