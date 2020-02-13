0e082b777f17842dc05e32ada51c6c33004cb448
cp3 <792716300@qq.com>
2020-02-13 17:12:47 +0800

    一步之遥

--- src/练习/一步之遥_欧几里得扩展.java
+++ src/练习/一步之遥_欧几里得扩展.java
@@ -21,9 +21,39 @@ public class 一步之遥_欧几里得扩展 {
 	 * 求第一个解
 	 * 
 	 */
+	static long x;
+	static long y;
 	public static void main(String[] args) {
-		
+		try {
+			linearEquation(97, -127, 1);
+			System.out.println(x+" "+y);
+			System.out.println(Math.abs(x)+Math.abs(y));//求绝对值
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+	}
+	public static long ext_gcd(long a,long b) {
+		if(b==0) {
+			x=1;
+			y=0;
+			return a;
+		}
+		long res=ext_gcd(b,a%b);
+		long x1=x;
+		x=y;
+		y=x1-a/b*y;
+		return res;
+	}
+	
+	public static long linearEquation(long a,long b,long m) throws Exception {
+		long d=ext_gcd(a,b);
+		if(m%d!=0)throw new Exception("无解");
+		long n=m/d;
+		x*=n;
+		y*=n;
+		return d;
 	}
 	
 	
