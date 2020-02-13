764a648520ab7de3337d90c028fad3c52e466c72
cp3 <792716300@qq.com>
2020-02-13 16:35:01 +0800

    欧几里得算法得扩展

--- src/第六章/裴蜀等式.java
+++ src/第六章/裴蜀等式.java
@@ -26,9 +26,55 @@ public class 裴蜀等式 {
 	 * 所以a%b=a-(a/b)*b
 	 * 
 	 */
+	
+	/**
+	 * 扩展欧几里得算法就是在求a,b的最大公约数m=gcd(a,b)的同余时，求出贝祖等式ax+by=m的一个解 (x0,y0)
+	 * 如何递推
+	 * x=y1
+	 * y=x1-a/b*y1
+	 * 
+	 * 通解：
+	 * x=x0+(b/gcd)*t
+	 * y=y0-(a/gcd)*t
+	 * 
+	 * 
+	 * 
+	 */
+	static long x;
+	static long y;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+	
+	/**
+	 * 
+	 * 扩展欧几里得
+	 * 调用完成后xy是ax+by=gcd(a,b)的解;
+	 * 
+	 * 
+	 */
+	public static long ext_gcd(long a,long b) {
+		if(b==0) {
+			x=1;
+			y=0;
+			return a;
+		}
+		long res=ext_gcd(b,a%b);
+		//x，y已经被下一层更新了
+		long x1=x;//备份x
+		x=y;//更新x
+		y=x1-a/b*y;//更新y
+		return res;
+	}
+	public static long linearEquation(long a,long b,long m) throws Exception {
+		long d=ext_gcd(a,b);
+		//m不是gcd(a,b)的倍数，此方程无解
+		if(m%d!=0)throw new Exception("无解");
+		long n=m/d;
+		x*=n;
+		y*=n;
+		return d;
 	}
 
 }
