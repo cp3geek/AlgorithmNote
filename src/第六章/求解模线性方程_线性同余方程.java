3468ee9f48042729459755175722389976964438
cp3 <792716300@qq.com>
2020-02-13 18:38:20 +0800

    青蛙的约会

--- src/第六章/求解模线性方程_线性同余方程.java
+++ src/第六章/求解模线性方程_线性同余方程.java
@@ -2,8 +2,17 @@ package 第六章;
 
 public class 求解模线性方程_线性同余方程 {
 
+	
+	/**
+	 * 用扩展欧几里得算法求解模线性方程的方法：
+	 * 同余方程组ax≡b(mod n)对于未知数x有解,当且仅当b是gcd(a,n)的倍数。且方程有解时
+	 * 方程有gcd(a,n)个解
+	 * 求解方程ax≡b(mod n)相当于求解ax+ny=b
+	 * ax看作整体
+	 * ax=ny+b
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
---
+++ src/第六章/青蛙的约会.java
@@ -0,0 +1,10 @@
+package 第六章;
+
+public class 青蛙的约会 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
