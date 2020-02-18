9825424d212ea23c139ddaf092d8bbf6feed3698
cp3 <792716300@qq.com>
2020-02-18 17:20:19 +0800

    硬币组合方式(递归形式)

--- src/第七章/硬币的组合方式.java
+++ src/第七章/硬币的组合方式.java
@@ -7,21 +7,26 @@ public class 硬币的组合方式 {
 	 * 
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(countWays(100));
 
 	}
 	
-	private int countWays(int n) {
+	private static int countWays(int n) {
 		if(n<=0)return 0;
 		return countWaysCore(n,new int[]{1,5,10,25},3);
 	}
 	
-	//cur代表当前最大面值
-	private int countWaysCore(int n,int[]coins,int cur) {
+	//cur代表当前最大面值,每次处理最大面值，剩余交给后面处理
+	private static int countWaysCore(int n,int[]coins,int cur){
+		if(cur==0)return 1;//用1来凑，永远只有一种，出口
+		int res=0;
+		//不取当前最大面值，取一个，取两个。。。具体可以循环几次，取决与n
+		//例如n等100，最大面值为25，则最大面值有5种取法（不取也算一种）0，1，2，3，4
 		for(int i=0;i*coins[cur]<=n;i++) {
-			
+			int shengyu=n-i*coins[cur];
+			res+=countWaysCore(shengyu, coins, cur-1);//累加结果
 		}
-		return 1;
+		return res;
 	}
 
 }
