f7583deb1adb6a84bf49037057b424838afe5d8d
cp3 <792716300@qq.com>
2020-02-20 15:20:10 +0800

    子集生成(递归)

--- src/第七章/子集生成.java
+++ src/第七章/子集生成.java
@@ -1,5 +1,8 @@
 package 第七章;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class 子集生成 {
 
 	/**
@@ -7,8 +10,40 @@ public class 子集生成 {
 	 * 
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[]A= {1,2,3};
+		Set<Set<Integer>>set=f(A,A.length,A.length-1);
+		for(Set<Integer>nset:set) {
+			if(!nset.isEmpty()) {
+				System.out.println(nset);
+			}
+		}
+		
+		
+		
 
+	}
+	
+	public static Set<Set<Integer>> f(int[]A,int n,int cur){
+		Set<Set<Integer>>newSet=new HashSet<Set<Integer>>();
+		if(cur==0) {
+			Set<Integer>first=new HashSet<Integer>();
+			Set<Integer>nil=new HashSet<Integer>();
+			
+			first.add(A[0]);
+			newSet.add(first);
+			newSet.add(nil);
+			return newSet;
+		}
+		Set<Set<Integer>>oldSet=f(A,n,cur-1);
+		for(Set<Integer>set:oldSet) {
+			newSet.add(set);
+			Set<Integer> clone=(Set<Integer>)((HashSet)set).clone();
+			clone.add(A[cur]);
+			newSet.add(clone);
+		}
+		
+		return newSet;
+		
 	}
 
 }
