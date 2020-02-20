413d1606f9ed3bf90373fb06513d06b825fd091c
cp3 <792716300@qq.com>
2020-02-20 16:28:59 +0800

    子集生成(二进制法)

--- src/第七章/子集生成之二进制解法.java
+++ src/第七章/子集生成之二进制解法.java
@@ -1,10 +1,30 @@
 package 第七章;
 
+import java.util.ArrayList;
+
 public class 子集生成之二进制解法 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[]A= {1,2,3};
+		ArrayList<ArrayList<Integer>>list=f(A,A.length);
+		System.out.println(list);
 
+	}
+	
+	public static ArrayList<ArrayList<Integer>>f(int[]A,int n){
+		ArrayList<ArrayList<Integer>>list=new ArrayList<ArrayList<Integer>>();
+		
+		
+		for(int i=(int)Math.pow(2, n)-1;i>0;i--) {
+			ArrayList<Integer>newlist=new ArrayList<Integer>();
+			for(int j=n-1;j>=0;j--) {
+				if(((i>>j)&1)==1) {
+					newlist.add(A[j]);
+				}
+			}
+			list.add(newlist);
+		}
+		return list;
 	}
 
 }
