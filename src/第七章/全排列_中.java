9d678301fc59acc88b81f936013474468d42854f
cp3 <792716300@qq.com>
2020-02-20 17:40:51 +0800

    全排列_中

--- src/第七章/全排列_中.java
+++ src/第七章/全排列_中.java
@@ -3,8 +3,27 @@ package 第七章;
 public class 全排列_中 {
 
 	public static void main(String[] args) {
-		
+		f(new char[] {'a','b','c'},0);
 
+	}
+	
+	public  static void f(char[]arr,int k) {
+		
+		if(k==arr.length) {
+			System.out.println(new String(arr));
+		}
+		
+		for(int i=k;i<arr.length;i++) {
+			swap(arr,i,k);//交换每一位到第一位
+			f(arr,k+1);
+			swap(arr,i,k);//回溯
+		}
+	}
+	
+	public static void swap(char[]arr,int i,int j) {
+		char c=arr[i];
+		arr[i]=arr[j];
+		arr[j]=c;
 	}
 
 }
