fe60b4ec9187af72d5943b7cddb89fdb1bd3e2ec
cp3 <792716300@qq.com>
2020-02-20 18:11:44 +0800

    全排列

--- src/第七章/全排列_下.java
+++ src/第七章/全排列_下.java
@@ -1,16 +1,61 @@
 package 第七章;
 
+import java.util.Arrays;
+
 public class 全排列_下 {
 
 	//刚才的方法无法维持字典序
 	
 	/**
-	 * 
+	 * n个数的排列组合找出字典序第k个排列
+	 * abc
+	 * acb
+	 * bac
+	 * bca
+	 * cab
+	 * cba
 	 * 
 	 */
+	final static int k=3;
+	static int count=0;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		f("",new char[] {'b','a','c'});
 
+	}
+	
+	public static void f(String prefix,char[]arr) {
+		Arrays.sort(arr);
+		if(prefix.length()==arr.length) {
+			System.out.println(prefix);
+			//用set去重
+			count++;
+			if(count==k) {
+				System.out.println("------"+prefix);
+				System.exit(0);//找到第k个排列直接退出
+			}
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			char ch=arr[i];
+			if(count(prefix,ch)<count(arr,ch)) {
+				f(prefix+ch,arr);
+			}
+		}
+	}
+	//处理重复
+	static int count(char[]arr,char ch) {
+		int cnt=0;
+		for(char c:arr) {
+			if(c==ch) cnt++;
+		}
+		return cnt;
+	}
+	static int count(String prefix,char ch) {
+		int cnt=0;
+		for(int i=0;i<prefix.length();i++) {
+			if(prefix.charAt(i)==ch)cnt++;
+		}
+		return cnt;
 	}
 
 }
--- src/第七章/全排列_中.java
+++ src/第七章/全排列_中.java
@@ -1,16 +1,21 @@
 package 第七章;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class 全排列_中 {
 
 	public static void main(String[] args) {
-		f(new char[] {'a','b','c'},0);
+		f(new char[] {'a','b','a'},0);
+		System.out.println(set);
 
 	}
-	
+	static Set<String>set=new HashSet<>();
 	public  static void f(char[]arr,int k) {
-		
+
 		if(k==arr.length) {
-			System.out.println(new String(arr));
+			set.add(new String(arr));
 		}
 		
 		for(int i=k;i<arr.length;i++) {
