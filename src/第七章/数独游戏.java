4b19fb696954fead1aa332414689516cb807ff66
cp3 <792716300@qq.com>
2020-02-22 18:04:53 +0800

    数独游戏

--- src/第七章/数独游戏.java
+++ src/第七章/数独游戏.java
@@ -1,5 +1,7 @@
 package 第七章;
 
+import java.util.Scanner;
+
 public class 数独游戏 {
 	
 	/**
@@ -27,13 +29,66 @@ public class 数独游戏 {
 	 * 
 	 * 
 	 * 程序应该输出：
+	 * 	145327698
+		839654127
+		672918543
+		496185372
+		218473956
+		753296481
+		367542819
+		984761235
+		521839764
 	 * 
 	 * 
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input =new Scanner(System.in);
+		char[][]table =new char[9][];
+		for(int i=0;i<9;i++) {
+			table[i]=input.nextLine().toCharArray();
+		}
+		dfs(table,0,0);
 
 	}
+	public static void dfs(char[][]table,int x,int y) {
+		if(x==9) {
+			print(table);
+			System.exit(0);
+		}
+		if(table[x][y]=='0') {
+			for(int i=1;i<=9;i++) {
+				if(check(table,x,y,i)) {
+					table[x][y]=(char)(i+'0');
+					dfs(table,x+(y+1)/9,(y+1)%9);
+				}
+			}
+			table[x][y]='0';
+			
+		}else {
+			dfs(table,x+(y+1)/9,(y+1)%9);
+		}
+	}
+	private static boolean check(char[][] table, int i, int j, int k) {
+		for(int l=0;l<9;l++) {
+			if(table[i][l]==(char)('0'+k))return false;
+			if(table[l][j]==(char)('0'+k))return false;
+		}
+		
+		for(int l=(i/3)*3;l<(i/3+1)*3;l++) {
+			for(int m=(j/3)*3;m<(j/3+1)*3;m++) {
+				if(table[l][m]==(char)('0'+k))return false;
+			}
+		}
+		
+		return true;
+	}
+	private static void print(char[][] table) {
+		for(int i=0;i<table.length;i++) {
+			System.out.println(new String(table[i]));
+		}
+		
+	}
+	
 
 }
