0c0ecb64f859fb874f4a18e4967be21d33fa5f4e
cp3 <792716300@qq.com>
2020-02-23 17:38:50 +0800

    水洼数

--- src/第七章/水洼数.java
+++ src/第七章/水洼数.java
@@ -61,17 +61,28 @@ W.W.W.....W.
 
 	private static void dfs(char[][] a, int i, int j) {
 		a[i][j]='.';
+//		for(int k=-1;k<2;k++) {//八个方向走其实就是坐标+1，-1，0不动
+//			for(int l=-1;l<2;l++) {
+//				if(k==0&&l==0)continue;
+//				
+//				if(i+k>=0&&i+k<=n-1&&j+l>=0&&j+l<=m-1) {
+//					if(a[i+k][j+l]=='W')
+//						dfs(a,i+k,j+l);
+//				}
+//			}
+//		}
 		for(int k=-1;k<2;k++) {
 			for(int l=-1;l<2;l++) {
 				if(k==0&&l==0)continue;
 				
 				if(i+k>=0&&i+k<=n-1&&j+l>=0&&j+l<=m-1) {
-					if(a[i+k][j+l]=='W')
+					if(a[i+k][j+l]=='W') {
 						dfs(a,i+k,j+l);
+					}
 				}
+				
 			}
 		}
-		
 		
 	}
 	/**
