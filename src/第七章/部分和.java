85b148eb44d696732d80b9aef9aa5b6b8278d7ad
cp3 <792716300@qq.com>
2020-02-23 15:10:39 +0800

    部分和

--- src/第七章/部分和.java
+++ src/第七章/部分和.java
@@ -2,8 +2,18 @@ package 第七章;
 
 public class 部分和 {
 
+	/**
+	 * 给定整数序列a1,a2,a3...an,判断是否可以从中选出若干数
+	 * 使他们的和恰好为k
+	 * 输入：
+	 * n=4
+	 * a={1,2,4,7}
+	 * k=13
+	 * 输出：
+	 * yes(13=2+4+7)
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
