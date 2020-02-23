63e77ce290a61d82f21a92bde1dc6f2638cc180c
cp3 <792716300@qq.com>
2020-02-23 16:06:44 +0800

    部分和(dfs)

--- src/第七章/部分和_dfs.java
+++ src/第七章/部分和_dfs.java
@@ -1,10 +1,19 @@
 package 第七章;
 
+import java.util.Scanner;
+
 public class 部分和_dfs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[]a=new int[n];
+		for(int i=0;i<n;i++)a[i]=sc.nextInt();
+		int k=sc.nextInt();
+		dfs(a,k);
+	}
+	public static void dfs(int[]a ,int k) {
+		
 	}
 
 }
