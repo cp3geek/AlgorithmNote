ad8b13115201944bb1ee1cc064c0e0a3f96dad26
cp3 <792716300@qq.com>
2020-02-23 17:31:07 +0800

    水洼数

--- src/第七章/水洼数.java
+++ src/第七章/水洼数.java
@@ -61,24 +61,17 @@ W.W.W.....W.
 
 	private static void dfs(char[][] a, int i, int j) {
 		a[i][j]='.';
-//		for(int k=-1;k<2;k++) {
-//			for(int l=-1;l<2;l++) {
-//				if(k==0&&l==0)continue;
-//				
-//				if(i+k>=0&&i+k<=n-1&&j+l>=0&&j+l<=m-1) {
-//					if(a[i+k][j+l]=='W')
-//						dfs(a,i+k,j+l);
-//				}
-//			}
-//		}
-		if(i>0&&a[i-1][j]=='W')dfs(a,i-1,j);//上
-		if(i<a.length-1&&a[i+1][j]=='W')dfs(a,i+1,j);//下
-		if(j<a[0].length-1&&a[i][j+1]=='W')dfs(a,i,j+1);//右
-		if(j>0&&a[i][j-1]=='W')dfs(a,i,j-1);//左
-		if(i>0&&j>0&&a[i-1][j-1]=='W')dfs(a,i-1,j-1);//左上角
-		if(j<a[0].length-1&&i>0&&a[i-1][j+1]=='W')dfs(a,i-1,j+1);//右上角
-		if(j>0&&i<a.length-1&&a[i+1][j-1]=='W')dfs(a,i+1,j-1);//左下角
-		if(j<a[0].length-1&&i<a.length-1&&a[i+1][j+1]=='W')dfs(a,i+1,j+1);//右下角
+		for(int k=-1;k<2;k++) {
+			for(int l=-1;l<2;l++) {
+				if(k==0&&l==0)continue;
+				
+				if(i+k>=0&&i+k<=n-1&&j+l>=0&&j+l<=m-1) {
+					if(a[i+k][j+l]=='W')
+						dfs(a,i+k,j+l);
+				}
+			}
+		}
+		
 		
 	}
 	/**
