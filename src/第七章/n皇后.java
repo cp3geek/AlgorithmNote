02e653041c3cd382233e63d97705e775ebf19a83
cp3 <792716300@qq.com>
2020-02-26 16:07:37 +0800

    n皇后

--- src/第七章/n皇后.java
+++ src/第七章/n皇后.java
@@ -12,9 +12,48 @@ public class n皇后 {
 	 *部分和里面有剪枝
 	 * 
 	 */
+	
+	/**
+	 * 请设计一种算法，解决著名的n皇后问题。这里n皇后问题指在一个n*n的棋盘上
+	 * 放置n个棋子，使得每行每列和每条对角线上都只有一个棋子，求其摆放的方法数
+	 * 给定一个int n，请返回方法数，保证n小于15
+	 * 
+	 * 技巧：如何检查对角线上的元素
+	 * 主对角线：x-y相同//行减列
+	 * 副对角线：x+y相同
+	 * 扫描rec数组：记录填的坐标，
+	 * 
+	 */
+	static int n;
+	static int cnt;
+	static int[]rec;
 	public static void main(String[] args) {
-		
+		n=4;
+		rec=new int[4];
+		dfs(0);
+		System.out.println(cnt);
 
 	}
+	public static void dfs(int row) {
+		if(row==n) {
+			cnt++;
+			return;
+		}
+		for(int col=0;col<n;col++) {
+			boolean ok=true;
+			for(int i=0;i<row;i++) {
+				if(rec[i]==col||i+rec[i]==row+col||i-rec[i]==row-col) {
+					ok=false;
+					break;
+				}
+			}
+			if(ok) {
+				rec[row]=col;
+				dfs(row+1);
+			}
+		}
+		
+	}
+	
 
 }
