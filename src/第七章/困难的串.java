2684fbc71b06f47a9bbbfe20904e76f149d2c7aa
cp3 <792716300@qq.com>
2020-02-26 17:56:57 +0800

    困难的串

--- src/第七章/困难的串.java
+++ src/第七章/困难的串.java
@@ -2,8 +2,20 @@ package 第七章;
 
 public class 困难的串 {
 
+	/**
+	 * 问题描述：如果一个字符串包含两个相邻的重复子串，则称它为容易的串，其他串
+	 * 称为困难的串
+	 * 如：BB ABCDACABCAB ABCDABCD都是容易的串，D DC,ABDAB,CBABCBA都是
+	 * 困难的串
+	 * 输入正整数n,L,输出由前L个字符组成的，字典序第n小的串的困难的串。
+	 * 例如当L=3时，前7个困难的串分别为：
+	 * A AB ABA ABAC ABACA ABACAB ABACABA
+	 * n指定为4的话，输出ABAC
+	 * 
+	 * 
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
