ba8b8e5b1f1586e3dd1822b2c3dabfe6d2a5e3f7
cp3 <792716300@qq.com>
2020-02-26 18:07:16 +0800

    困难的串

--- src/第七章/困难的串.java
+++ src/第七章/困难的串.java
@@ -7,12 +7,12 @@ public class 困难的串 {
 	 * 称为困难的串
 	 * 如：BB ABCDACABCAB ABCDABCD都是容易的串，D DC,ABDAB,CBABCBA都是
 	 * 困难的串
-	 * 输入正整数n,L,输出由前L个字符组成的，字典序第n小的串的困难的串。
+	 * 输入正整数n,L,输出由前L个字符(26个大写英文字母)组成的，字典序第n小的串的困难的串。
 	 * 例如当L=3时，前7个困难的串分别为：
 	 * A AB ABA ABAC ABACA ABACAB ABACABA
 	 * n指定为4的话，输出ABAC
 	 * 
-	 * 
+	 * 不是很理解，明天再看一遍
 	 */
 	public static void main(String[] args) {
 		
