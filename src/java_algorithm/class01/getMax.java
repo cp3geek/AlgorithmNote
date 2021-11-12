20b849034d3e61c7400973be2e53b466014ca165
cp3 <792716300@qq.com>
2021-11-12 23:00:38 +0800

    getMax

--- src/java_algorithm/class01/getMax.java
+++ src/java_algorithm/class01/getMax.java
@@ -2,8 +2,22 @@ package java_algorithm.class01;
 
 public class getMax {
 
-    public static int getMax(){
-        return 0;
+    public static int getMax(int[]arr){
+        return process(arr,0,arr.length-1);
+
+    }
+
+    public static int process(int[]arr,int L,int R){
+        if(R==L)return arr[L];
+        /**
+         * 求中点
+         * mid=(R+L)/2,但可能会溢出
+         * mid=L+(R-L)/2
+         */
+        int mid=L+((R-L)>>1);
+        int leftMax=process(arr,L,mid);
+        int rightMax=process(arr,mid+1,R);
+        return Math.max(leftMax,rightMax);
     }
 
 
