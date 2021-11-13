923d7ef54bb9e31b380b58a1194dbf826fd64187
cp3 <792716300@qq.com>
2021-11-13 13:09:18 +0800

    mergeSort

--- src/java_algorithm/class01/MergeSort.java
+++ src/java_algorithm/class01/MergeSort.java
@@ -11,25 +11,23 @@ public class MergeSort {
         process(arr,0,arr.length-1);
     }
 
-    public static void process(int[]arr,int L,int R){
-        if(L==R)return;
-        int mid=L+((R-L)>>1);
-        process(arr,L,mid);
-        process(arr,mid+1,R);
-        merge(arr,L,mid,R);
-
+    public static void process(int[]arr,int l,int r){
+        if(l==r)return;
+        int mid=l+((r-l)>>1);
+        process(arr,l,mid);
+        process(arr,mid+1,r);
+        merge(arr,l,mid,r);
 
     }
-
-    private static void merge(int[] arr, int l, int mid, int r) {
-        int[] help=new int[r-l+1];
+    public static void merge(int[]arr,int l,int m,int r){
+        int[]help=new int[r-l+1];
         int i=0;
         int p1=l;
-        int p2=mid+1;
-        while(p1<=mid&&p2<=r){
-            help[i++]=arr[p1]<arr[p2]?arr[p1++]:arr[p2++];
-        }
-        while(p1<=mid)help[i++]=arr[p1++];
+        int p2=m+1;
+
+
+        while(p1<=m&&p2<=r)help[i++]=arr[p1]<arr[p2]?arr[p1++]:arr[p2++];
+        while(p1<=m)help[i++]=arr[p1++];
         while(p2<=r)help[i++]=arr[p2++];
         for(i=0;i<help.length;i++)arr[l+i]=help[i];
 
