0cc4de40735eab6d9a8c31b5f1cc1c20babba2eb
cp3 <792716300@qq.com>
2021-11-15 20:23:07 +0800

    heapSort

--- src/java_algorithm/class01/HeapSort.java
+++ src/java_algorithm/class01/HeapSort.java
@@ -5,20 +5,31 @@ public class HeapSort {
 
     }
 
+
+    public static void heapSort(int[]arr){
+        if(arr==null||arr.length<2)return;
+        for(int i=0;i<arr.length;i++)heapInsert(arr,i);
+        int heapSize=arr.length;
+        swap(arr,0,--heapSize);
+        while(heapSize>0){
+            heapify(arr,0,heapSize);
+            swap(arr,0,--heapSize);
+        }
+    }
+
     //某个数在index位置，能否往下移动
     public static void heapify(int[]arr,int index,int heapSize){
         int left=index*2+1;//左孩子下标
         while(left<heapSize){//下方还有孩子的时候
             //两个孩子中，谁的值大，就把下标给largest
             int largest=left+1<heapSize&&arr[left+1]>arr[left]?left+1:left;
-            //父和孩子之间，谁的值大，把下标给largest
+            //父和较大孩子之间，谁的值大，把下标给largest
             largest=arr[largest]>arr[index]?largest:index;
             if(largest==index)break;
 
             swap(arr,largest,index);
-            index=largest;
+            index=largest;//当前数往下走
             left=index*2+1;
-
         }
     }
 
