5eb58376843227398f0fe1cea96e9d4d6aa1ea36
cp3 <792716300@qq.com>
2021-11-18 15:48:03 +0800

    HashAndTree

--- src/java_algorithm/class02/HashAndTree.java
+++ src/java_algorithm/class02/HashAndTree.java
@@ -1,6 +1,9 @@
 package java_algorithm.class02;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class HashAndTree {
@@ -22,11 +25,33 @@ public class HashAndTree {
         Node nodeA=null;
         Node nodeB=null;
         Node nodeC=null;
-
+        //set只有key，map既有key也有value
+        //hashSet1的key是基础类型 -> int类型
         HashSet<Integer>hashSet1=new HashSet<>();
         hashSet1.add(3);
         System.out.println(hashSet1.contains(3));
         hashSet1.remove(3);
         System.out.println(hashSet1.contains(3));
+
+
+        HashMap<Integer,String>mapTest=new HashMap<>();
+        mapTest.put(1,"zuo");
+        mapTest.put(2,"cheng");
+        mapTest.put(2,"2");//key不能重复
+        System.out.println(mapTest.containsKey(1));
+        System.out.println(mapTest.get(1));
+        System.out.println(mapTest.get(2));
+
+        mapTest.remove(2);
+        System.out.println(mapTest.get(2));
+        System.out.println("===============================================");
+
+        //hashSet2的key是非基础类型->Node类型
+
+        nodeA=new Node(1);
+        nodeB=new Node(1);
+        HashSet<Node>hashSet2=new HashSet<>();
+        
+
     }
 }
