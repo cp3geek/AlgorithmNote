db98bd624f1b4ef8978e22e70013e59a8861d451
cp3 <792716300@qq.com>
2021-11-18 14:54:09 +0800

    RadixSort

--- src/java_algorithm/class01/RadixSort.java
+++ src/java_algorithm/class01/RadixSort.java
@@ -24,22 +24,28 @@ public class RadixSort {
         final int radix=10;
         int i=0,j=0;
         int[]bucket=new int[R-L+1];
-        for(int d=1;d<=digit;d++){
-            int[]count=new int[radix];
+        for(int d=1;d<=digit;d++){//出桶进桶次数由十进制位数决定
+            //10个空间
+            //count[0]当前位（d位）是0的数字有多少个
+            //count[1]当前位（d位）是（0、1）的数字有多少个
+            //count[2]当前位（d位）是（0、1、2）的数字有多少个
+            //count[i]当前位（d位）是（0-i）的数字有多少个
+            int[] count=new int[radix];
             for(i=L;i<=R;i++){
-                j=getDigit(arr[i],d);
-                count[j++];
+                j=getDigit(arr[i],d);//d如果是1，就取出个位数字，如果是2就取出十位数字，如果是3就取出百位数字
+                count[j]++;
             }
-            for(i=1;i<radix;i++) count[i]=count[i]+count[i-1];
-            for(i=R;i>=L;i--){
-                j=getDigit(arr[i],d);
-                bucket[count[j]-1]=arr[i];
-                count[j--];
-            }
+            for(i=1;i<radix;i++) count[i]=count[i]+count[i-1];//然后将count数组处理成前缀和
+            for(i=R;i>=L;i--){//然后从右往左遍历数组
+                j=getDigit(arr[i],d);//拿出当前数的d位上的数
+                bucket[count[j]-1]=arr[i];//根据count数组填入到辅助数组中
+                count[j]--;
+            }//这一步所有数字出桶
             for(i=L,j=0;i<=R;i++,j++)arr[i]=bucket[j];
         }
     }
 
     private static int getDigit(int i, int d) {
+        return 0;
     }
 }
