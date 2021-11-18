df5c374c01de01607dd342588de22fe8182c57ea
cp3 <792716300@qq.com>
2021-11-18 19:21:00 +0800

    回文链表代码补充三种方法

--- src/java_algorithm/class02/PalindromeLinkedList.java
+++ src/java_algorithm/class02/PalindromeLinkedList.java
@@ -59,6 +59,40 @@ public class PalindromeLinkedList {
     public static boolean isPalindrome3(Node head){
         if(head==null||head.next==null)return false;
 
+        Node n1=head;//慢指针
+        Node n2=head;//快指针
+        while(n2.next!=null&&n2.next.next!=null){
+            n1=n1.next;//一次走一步
+            n2=n2.next.next;//一次走两步
+        }
+        n2=n1.next;
+        n1.next=null;
+        Node n3=null;
+        while (n2!=null){//右半部分逆序
+            n3=n2.next;
+            n2.next=n1;
+            n1=n2;
+            n2=n3;
+        }
+        n3=n1;
+        n2=head;
+        boolean res=true;
+        while (n1!=null&&n2!=null){
+            if(n1.value!=n2.value){
+                res=false;
+                break;
+            }
+            n1=n1.next;//left to mid
+            n2=n2.next;//right to mid
+        }
+        n1=n3.next;
+        n3.next=null;
+        while (n1!=null){
+            n2=n1.next;
+            n1.next=n3;
+            n3=n1;
+            n1=n2;
+        }
         return true;
     }
 
